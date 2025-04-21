0d97d9a6e19f0d0b6d6a89dcb1459472217ef7ec
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-04-20 23:16:46 -0500

    Time: 3 ms (9.39%) | Memory: 17.8 MB (52.85%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,17 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        hashTable = {}
-
-        for jew in jewels:
-            hashTable[jew] = 0
-
-        for stone in stones:
-            if stone in hashTable:
-                hashTable[stone] += 1
-
         result = 0
 
-        for key, value in hashTable.items():
-            result += value
+        for stone in stones:
+            if stone in jewels:
+                result += 1
 
         return result
