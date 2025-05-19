007dc098ac4e21182a5d009d3758b70007aff61a
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-05-19 14:49:06 -0500

    Time: 427 ms (93.70%) | Memory: 20.7 MB (69.01%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
+        n = len(nums)
+        sort_nums = sorted(nums)
         result = []
 
-        for i in range(len(nums)-1):
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in range(n):
+            if i > 0 and sort_nums[i] == sort_nums[i-1]:
                 continue
-            elif nums[i] > 0:
+            elif sort_nums[i] > 0:
                 break
-
-            low = i+1
-            high = len(nums)-1
-
-            while low < high:
-                if nums[i] + nums[low] + nums[high] < 0:
-                    low += 1
-                elif nums[i] + nums[low] + nums[high] > 0:
-                    high -= 1
+            
+            left = i+1
+            right = n-1
+            while left < right:
+                total = sort_nums[i] + sort_nums[left] + sort_nums[right]
+                if total > 0: 
+                    right -= 1
+                elif total < 0:
+                    left += 1
                 else:
-                    result.append([nums[i], nums[low], nums[high]])
-                    low += 1
-                    high -= 1
+                    result.append([sort_nums[i], sort_nums[left], sort_nums[right]])
+                    left += 1
+                    right -= 1
 
-                    while low < high and nums[low] == nums[low-1]:
-                        low += 1
-                    while low < high and nums[high] == nums[high+1]:
-                        high -= 1
+                    while left < n and sort_nums[left] == sort_nums[left-1]:
+                        left += 1
+                    while right > 0 and sort_nums[right] == sort_nums[right+1]:
+                        right -= 1
 
         return result
