6c5f83ed9d8bc74b6e04b447a4ddd7c723283247
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-05 13:37:55 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (22.94%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -3,14 +3,14 @@ class Solution:
         if not digits:
             return []
 
-        dic = { 2: 'abc',
-                3: 'def',
-                4: 'ghi',
-                5: 'jkl',
-                6: 'mno',
-                7: 'pqrs',
-                8: 'tuv',
-                9: 'wxyz'}
+        dic = { '2': 'abc',
+                '3': 'def',
+                '4': 'ghi',
+                '5': 'jkl',
+                '6': 'mno',
+                '7': 'pqrs',
+                '8': 'tuv',
+                '9': 'wxyz'}
 
         n = len(digits)
         result, temp = [], []
@@ -20,14 +20,10 @@ class Solution:
                 result.append(''.join(temp))
                 return
 
-            for x in range(i, n):
-                num = int(digits[x])
-                letters = dic[num]
-
-                for word in letters:
-                    temp.extend(word)
-                    backtrack(x+1)
-                    temp.pop()
+            for letter in dic[digits[i]]:
+                temp.append(letter)
+                backtrack(i+1)
+                temp.pop()
 
         backtrack(0)
         return result
