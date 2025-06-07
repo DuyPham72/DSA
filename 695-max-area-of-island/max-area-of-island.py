3c171fdc07877b5784b5d660e2658f6ca1472548
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-06 22:17:45 -0500

    Time: 15 ms (89.15%) | Memory: 18.2 MB (79.56%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -8,18 +8,12 @@ class Solution:
                 return 0
             
             grid[i][j] = 0
-            
-            right = dfs(i, j+1)
-            left = dfs(i, j-1)
-            down = dfs(i+1, j)
-            up = dfs(i-1, j)
-            return 1 + right + down + left + up
+            return 1 + dfs(i, j+1) + dfs(i, j-1) + dfs(i+1, j) + dfs(i-1, j)
 
         result = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    temp = dfs(i, j)
-                    result = max(result, temp)
+                    result = max(result, dfs(i, j))
 
         return result
