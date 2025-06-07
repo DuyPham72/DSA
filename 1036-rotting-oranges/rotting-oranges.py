b927d6d85252f37a06814b5a8d511b1053d0f73c
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-07 12:53:49 -0500

    Time: 2 ms (86.86%) | Memory: 17.7 MB (71.04%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,25 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        g = deque()
+        m, n = len(grid), len(grid[0])
         fresh = 0
+        queue = deque()
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
-                    g.append((i,j))
-                elif grid[i][j] == 1:
+                if grid[i][j] == 1:
                     fresh += 1
+                elif grid[i][j] ==  2:
+                    queue.append((i, j))
 
         minute = 0
-        while g and fresh > 0:
-            for _ in range(len(g)):
-                i, j = g.popleft()
-                for x, y in [(0,1), (0, -1), (1, 0), (-1, 0)]:
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                i, j = queue.popleft()
+                for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                     r, c = i+x, j+y
                     if 0 <= r < m and 0 <= c < n and grid[r][c] == 1:
                         grid[r][c] = 2
-                        g.append((r, c))
+                        queue.append((r, c))
                         fresh -= 1
 
             minute += 1
