49e5a33ce6d80e893cea31bed72c5e97806831c0
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-08 22:27:18 -0500

    Time: 3 ms (52.74%) | Memory: 18.2 MB (39.62%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,26 +1,28 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            return self.findMedianSortedArrays(nums2, nums1)
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total //2
 
-        len1, len2 = len(nums1), len(nums2)
-        left, right = 0, len1
+        if len(B) < len(A):
+            A,B = B,A
+        
+        l, r = 0, len(A)-1
+        while True:
+            m = (l+r)//2
+            j = half - m - 2
 
-        while left <= right:
-            part1 = (left + right) // 2
-            part2 = (len1 + len2 + 1) // 2 - part1
+            Aleft = A[m] if m >= 0 else float('-infinity')
+            Aright = A[m+1] if m+1 < len(A) else float('infinity')
+            Bleft = B[j] if j >= 0 else float('-infinity')
+            Bright = B[j+1] if j+1 < len(B) else float('infinity')
 
-            max_left1 = float('-inf') if part1 == 0 else nums1[part1 - 1]
-            min_right1 = float('inf') if part1 == len1 else nums1[part1]
-            max_left2 = float('-inf') if part2 == 0 else nums2[part2 - 1]
-            min_right2 = float('inf') if part2 == len2 else nums2[part2]
-
-            if max_left1 <= min_right2 and max_left2 <= min_right1:
-                if (len1 + len2) % 2 == 0:
-                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2 == 1:
+                    return min(Aright, Bright)
                 else:
-                    return max(max_left1, max_left2)
-            elif max_left1 > min_right2:
-                right = part1 - 1
+                    return (max(Aleft, Bleft) + min(Aright, Bright))/2
+            elif Aleft > Bright:
+                r = m-1
             else:
-                left = part1 + 1
+                l = m+1
