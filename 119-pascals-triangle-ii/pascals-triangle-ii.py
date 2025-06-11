73c5a45016828554b2e06338165312aa3b41ff26
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-11 17:28:19 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.59%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = []
+        for depth in range(1, rowIndex+2):
+            temp = [1] * depth
+            n = len(temp)
+            if n > 2:
+                for i in range(1, n-1):
+                    temp[i] = result[depth-2][i-1] + result[depth-2][i]
+            
+            result.append(temp[:])
+                
+        return result[rowIndex]
