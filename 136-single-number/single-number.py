fbb9130e0b612ad53974e387ad46563beebb4e1e
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-11 17:52:30 -0500

    Time: 9 ms (21.07%) | Memory: 19.8 MB (14.42%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashmap = {}
-        for num in nums:
-            if num not in hashmap:
-                hashmap[num] = 1
-            else:
-                hashmap[num] += 1
-
+        hashmap = Counter(nums)
         for key, value in hashmap.items():
             if value == 1:
                 return key
