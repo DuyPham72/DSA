70aaa0644c82a10e92e60e4c7f13d6d3d92496f2
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-12 17:59:44 -0500

    Time: 451 ms (88.41%) | Memory: 20.6 MB (68.94%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -8,21 +8,22 @@ class Solution:
             if i > 0 and sort[i] == sort[i-1]:
                 continue
             if sort[i] > 0:
-                break 
+                break
 
             l = i + 1
             r = n-1
             while l < r:
-                if sort[i] + sort[l] + sort[r] == 0:
+                total = sort[i] + sort[l] + sort[r]
+                if total == 0:
                     result.append([sort[i], sort[l], sort[r]])
                     l += 1
                     r -= 1
 
-                    while l < n and sort[l] == sort[l-1]:
+                    while l < r and sort[l] == sort[l-1]:
                         l+= 1
-                    while r > 0 and sort[r] == sort[r+1]:
+                    while l < r and sort[r] == sort[r+1]:
                         r -= 1
-                elif sort[i] + sort[l] + sort[r] < 0:
+                elif total < 0:
                     l += 1
                 else:
                     r -= 1
