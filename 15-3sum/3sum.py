6ee8bf64db39829b5680bcd26bb092389896c00d
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-12 17:57:14 -0500

    Time: 641 ms (33.03%) | Memory: 20.6 MB (68.94%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,31 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        sort_nums = sorted(nums)
+        sort = sorted(nums)
         result = []
 
         for i in range(n):
-            if i > 0 and sort_nums[i] == sort_nums[i-1]:
+            if i > 0 and sort[i] == sort[i-1]:
                 continue
-            elif sort_nums[i] > 0:
-                break
-            
-            left = i+1
-            right = n-1
-            while left < right:
-                total = sort_nums[i] + sort_nums[left] + sort_nums[right]
-                if total > 0: 
-                    right -= 1
-                elif total < 0:
-                    left += 1
-                else:
-                    result.append([sort_nums[i], sort_nums[left], sort_nums[right]])
-                    left += 1
-                    right -= 1
+            if sort[i] > 0:
+                break 
 
-                    while left < n and sort_nums[left] == sort_nums[left-1]:
-                        left += 1
-                    while right > 0 and sort_nums[right] == sort_nums[right+1]:
-                        right -= 1
+            l = i + 1
+            r = n-1
+            while l < r:
+                if sort[i] + sort[l] + sort[r] == 0:
+                    result.append([sort[i], sort[l], sort[r]])
+                    l += 1
+                    r -= 1
+
+                    while l < n and sort[l] == sort[l-1]:
+                        l+= 1
+                    while r > 0 and sort[r] == sort[r+1]:
+                        r -= 1
+                elif sort[i] + sort[l] + sort[r] < 0:
+                    l += 1
+                else:
+                    r -= 1
 
         return result
