618bd969aa3d362880ba15415ee3c079d935c3eb
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-12 17:05:35 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (47.26%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -2,13 +2,17 @@ class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         max1 = max2 = max3 = None
 
+        seen = set()
         for num in nums:
-            if num != max1 and num != max2 and num != max3:
-                if max1 is None or num > max1:
-                    max1, max2, max3 = num, max1, max2
-                elif max2 is None or num > max2:
-                    max2, max3 = num, max2
-                elif max3 is None or num > max3:
-                    max3 = num
+            if num in seen:
+                continue
+            seen.add(num)
+
+            if max1 is None or num > max1:
+                max1, max2, max3 = num, max1, max2
+            elif max2 is None or num > max2:
+                max2, max3 = num, max2
+            elif max3 is None or num > max3:
+                max3 = num
 
         return max3 if max3 is not None else max1
