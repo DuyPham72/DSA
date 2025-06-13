33f7c688521943770920d430325ef7260fd18c50
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-13 16:53:37 -0500

    Time: 3 ms (43.09%) | Memory: 18.1 MB (39.68%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = {}
+        for idx, value in enumerate(nums2):
+            hashmap[value] = idx
+            
         result = []
-        for i in range(len(nums1)):
+        for num in nums1:
             ans = -1
-            idx = 0
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    idx = j
+            idx = hashmap.get(num)
 
-            for j in range(idx+1, len(nums2)):
-                if nums1[i] < nums2[j]:
+            for j in range(idx + 1, len(nums2)):
+                if num < nums2[j]:
                     ans = nums2[j]
                     break
 
