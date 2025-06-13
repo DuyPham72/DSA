c5ee8a7f5c39538e9bcdb98cdddf5ed8f10897fe
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-13 15:44:26 -0500

    Time: 27 ms (14.60%) | Memory: 18.9 MB (10.99%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.py
+++ 495-teemo-attacking/teemo-attacking.py
@@ -2,6 +2,7 @@ class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
         n = len(timeSeries)
         ans = 0
+        end = 0
         for i in range(n):
             t = timeSeries[i]
             end = t + duration
