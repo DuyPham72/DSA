7cfaa4089fb7868a84e0c21ef367c21f95078551
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-14 16:34:36 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.88%) - LeetSync

--- 500-keyboard-row/keyboard-row.py
+++ 500-keyboard-row/keyboard-row.py
@@ -1,21 +1,13 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        first = set("qwertyuiop")
-        second = set("asdfghjkl")
-        third = set("zxcvbnm")
+        rows = [ set("qwertyuiopQWERTYUIOP"), 
+                set("asdfghjklASDFGHJKL"), 
+                set("zxcvbnmZXCVBNM")]
 
         result = []
         for word in words:
-            count1 = count2 = count3 = 0
-            for letter in word:
-                if letter.lower() in first:
-                    count1 += 1
-                elif letter.lower() in second:
-                    count2 += 1
-                else:
-                    count3 += 1
-
-            if (count1 or count2 or count3) == len(word):
+            wset = set(word)
+            if any(wset <= row for row in rows):
                 result.append(word)
 
         return result                
