8058e607bdab59117090e436f7b94270f643f1ca
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-14 23:43:10 -0500

    Time: 2 ms (58.44%) | Memory: 19.7 MB (40.99%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -3,13 +3,9 @@ class Solution:
         n = len(intervals)
         result = []
         count = 0
-        for pair in intervals:
-            i, j = pair
-            if j < newInterval[0]:
-                result.append(pair)
-                count += 1
-            else:
-                break
+        while count < n and intervals[count][1] < newInterval[0]:
+            result.append(intervals[count])
+            count += 1
 
         while count < n and intervals[count][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[count][0])
