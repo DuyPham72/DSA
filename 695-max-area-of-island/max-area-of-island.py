aa646c9ce8900b4b0ee91b4cfe89485c8da44185
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-22 22:23:57 -0500

    Time: 13 ms (92.38%) | Memory: 18.4 MB (52.73%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        m, n = len(grid), len(grid[0])
 
-        def dfs(i, j):
+        def area(i, j):
             if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
                 return 0
-            
-            grid[i][j] = 0
-            return 1 + dfs(i, j+1) + dfs(i, j-1) + dfs(i+1, j) + dfs(i-1, j)
 
-        result = 0
+            grid[i][j] = 0
+            return 1 + area(i, j+1) + area(i, j-1) + area(i+1, j) + area(i-1, j)
+
+        max_area = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    result = max(result, dfs(i, j))
+                    max_area = max(max_area, area(i, j))
 
-        return result
+        return max_area
