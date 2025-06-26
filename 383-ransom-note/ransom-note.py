4c3f2a4fca8ece00a15aaaea44a1fb651b954e65
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-26 17:07:35 -0500

    Time: 7 ms (93.47%) | Memory: 18.2 MB (13.22%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,14 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        letter = Counter(magazine)
+        count = Counter(magazine)
 
-        for note in ransomNote:
-            if note not in letter:
+        for word in ransomNote:
+            if count.get(word, 0) == 0:
                 return False
-
-            if letter[note] == 0:
-                return False
-                
-            letter[note] -= 1
+            
+            count[word] -= 1
 
         return True
