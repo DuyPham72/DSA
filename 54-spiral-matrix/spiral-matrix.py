4694a3011e59f5fa3d6f25861abfe79add25ceca
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-25 21:11:01 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.11%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,46 +1,27 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        top, bot = 0, m
+        left, right = 0, n
+
         result = []
+        while top < bot and left < right:
+            for j in range(top, right):
+                result.append(matrix[top][j])
+            top += 1
 
-        row, column = len(matrix), len(matrix[0])
-        i, j = 0, 0
+            for i in range(top, bot):
+                result.append(matrix[i][right-1])
+            right -= 1
 
-        left, right, up, down = 0, 1, 2, 3
-        current_direction = right
+            if top < bot:
+                for j in range(right-1, left-1, -1):
+                    result.append(matrix[bot-1][j])
+                bot -= 1
 
-        top_wall = 0
-        right_wall = column
-        bot_wall = row
-        left_wall = -1
-
-        while len(result) != row*column:
-            if current_direction == right:
-                while j < right_wall:
-                    result.append(matrix[i][j])
-                    j += 1
-                i, j = i+1, j-1
-                right_wall -= 1
-                current_direction = down
-            elif current_direction == down:
-                while i < bot_wall:
-                    result.append(matrix[i][j])
-                    i += 1
-                i, j = i-1, j-1
-                bot_wall -= 1
-                current_direction = left
-            elif current_direction == left:
-                while j > left_wall:
-                    result.append(matrix[i][j])
-                    j -= 1
-                i, j = i-1, j+1
-                left_wall += 1
-                current_direction = up
-            else:
-                while i > top_wall:
-                    result.append(matrix[i][j])
-                    i -= 1
-                i, j = i+1, j+1
-                top_wall += 1
-                current_direction = right
-
+            if left < right:
+                for i in range(bot-1, top-1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+            
         return result
