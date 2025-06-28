c2c401aa3886a2fffab6410f7a0539f8678b1652
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-28 13:07:08 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (51.87%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,22 +6,22 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        n = len(inorder)
         hashmap = {val: i for i, val in enumerate(inorder)}
 
-        def helper(l_in, r_in, l_post, r_post):
+        def build(l_in, r_in, l_post, r_post):
             if l_in >= r_in or l_post >= r_post:
                 return
 
-            root_node = postorder[r_post - 1]
-            node = TreeNode(root_node)
+            root_val = postorder[r_post-1]
+            node = TreeNode(root_val)
 
-            mid = hashmap[root_node]
+            mid = hashmap[root_val]
             left_side = mid - l_in
 
-            node.left = helper(l_in, mid, l_post, l_post + left_side)
-            node.right = helper(mid+1, r_in, l_post + left_side, r_post-1)
+            node.left = build(l_in, mid, l_post, l_post + left_side)
+            node.right = build(mid+1, r_in, l_post + left_side, r_post-1)
 
             return node
 
-        return helper(0, n, 0, n)
+        n = len(inorder)
+        return build(0, n, 0, n)
