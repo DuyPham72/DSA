061aea9c226796eb5b273139c76251c46c3f4e25
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-06-29 19:45:54 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.84%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,24 +1,12 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        thousands = {
-            0: "", 1: "M", 2: "MM", 3: "MMM"
-        }
-        hundreds = {
-            0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD",
-            5: "D", 6: "DC", 7: "DCC", 8: "DCCC", 9: "CM"
-        }
-        tens = {
-            0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL",
-            5: "L", 6: "LX", 7: "LXX", 8: "LXXX", 9: "XC"
-        }
-        ones = {
-            0: "", 1: "I", 2: "II", 3: "III", 4: "IV",
-            5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX"
-        }
+        val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
-        thousand = num // 1000
-        hundred = (num % 1000) // 100
-        ten = (num % 100) // 10
-        one = num % 10
+        ans = ''
+        for i in range(len(val)):
+            while num >= val[i]:
+                ans += syms[i]
+                num -= val[i]
 
-        return thousands[thousand] + hundreds[hundred] + tens[ten] + ones[one]
+        return ans
