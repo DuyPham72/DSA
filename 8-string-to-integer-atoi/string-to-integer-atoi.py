b6fba8738c5fecb1d3303a5bd13fcd1d678d2e34
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-07-01 08:20:52 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (8.51%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,29 +1,24 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         i = 0
-        while i < len(s) and s[i] == ' ':
+        n = len(s)
+        while i < n and s[i] == ' ':
             i += 1
 
-        if len(s) == i:
+        if n == i:
             return 0
 
-        sign = False
+        sign = 1
         if s[i] == '-':
-            sign = True
+            sign = -1
             i += 1
         elif s[i] == '+':
             i += 1
 
-        valid = set('0123456789')
         ans = 0
-        while i < len(s) and s[i] in valid:
-            digit = int(s[i])
-            ans = ans * 10 + digit
+        while i < n and s[i].isdigit():
+            ans = ans * 10 + int(s[i])
             i += 1
 
-        ans = -ans if sign else ans
-        if ans < -2**31:
-            return -2**31
-        if ans > 2**31 - 1:
-            return 2**31 - 1
-        return ans
+        ans *= sign
+        return max(-2**31, min(ans, 2**31 - 1))
