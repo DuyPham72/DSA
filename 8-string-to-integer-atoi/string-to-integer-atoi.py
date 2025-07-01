11191d8c1c8274d5a2ee7314b2b58748a00ed364
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-07-01 08:13:03 -0500

    Time: 2 ms (46.76%) | Memory: 17.9 MB (28.73%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        i = 0
+        while i < len(s) and s[i] == ' ':
+            i += 1
+
+        if len(s) == i:
+            return 0
+
+        sign = False
+        if s[i] == '-':
+            sign = True
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        valid = set('0123456789')
+        ans = 0
+        while i < len(s) and s[i] in valid:
+            digit = int(s[i])
+            ans = ans * 10 + digit
+            i += 1
+
+        if ans == 0:
+            return 0
+
+        ans = -ans if sign else ans
+        if ans < -2**31:
+            return -2**31
+        if ans > 2**31 - 1:
+            return 2**31 - 1
+        return ans
