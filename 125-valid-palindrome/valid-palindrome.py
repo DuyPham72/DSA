60418e6c19c7f8ac25d09791f4d174eda643ea8d
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-07-09 19:29:22 -0500

    Time: 7 ms (81.30%) | Memory: 19.4 MB (22.59%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,16 @@
+import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        clean = ''.join(ch for ch in s if ch.isalnum()).lower()
-
-        left = 0
-        right = len(clean) - 1
-
-        while left <= right:
-            if clean[left] != clean[right]:
+        preprocess = re.sub(r'[^a-z0-9]', '', s.lower())
+        
+        l = 0
+        r = len(preprocess)-1
+        while l<r:
+            if preprocess[l] != preprocess[r]:
                 return False
-            else:
-                left += 1
-                right -= 1
+
+            l +=1 
+            r -= 1
 
         return True
