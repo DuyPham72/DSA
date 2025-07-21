f7aaf7ecb8116d8b9d6bea7ae69cfdbb2979ea45
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-07-21 18:19:19 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (88.72%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,16 +1,17 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        paper = [0]*(n+1)
+        ans = [0] * (n+1)
 
-        for c in citations:
-            paper[min(n, c)] += 1
+        for cite in citations:
+            ans[min(cite, n)] += 1
+
+        print(ans)
 
         h = n
-        paper_count = paper[h]
-
+        paper_count = ans[h]
         while h > paper_count:
             h -= 1
-            paper_count += paper[h]
+            paper_count += ans[h]
 
         return h
