c459cee3cdc97e462d235f13ec8510a2d4756243
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-08-10 18:20:25 -0500

    Time: 7 ms (39.77%) | Memory: 17.7 MB (89.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,23 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {
-            'I' : 1,
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
+        dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
 
-        ans = 0
-        prev = None
-        for num in s:
-            integer = roman[num]
-            if prev is not None and prev < integer:
-                ans -= 2*prev
+        res = 0
+        temp = None
+        for roman in s:
+            integer = dic[roman]
+            if temp != None and integer > temp:
+                res -= 2*temp
 
-            ans += integer
-            prev = integer
+            res += integer
+            temp = integer
 
-        return ans
+        return res
