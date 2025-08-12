0bff39db4010c329e910a6c56539dd2ecdf50803
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-08-12 14:26:17 -0500

    Time: 1 ms (31.28%) | Memory: 18.3 MB (20.22%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,7 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        ans = [0] * (n+1)
+        ans = [0]*(n+1)
 
         for cite in citations:
             ans[min(cite, n)] += 1
@@ -13,5 +13,5 @@ class Solution:
         while h > paper_count:
             h -= 1
             paper_count += ans[h]
-
+        
         return h
