ebaa468f7762e9e3ae16865515947e1ae6a5b2c3
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-08-14 15:31:09 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.28%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -4,24 +4,24 @@ class Solution:
         top, bot = 0, m
         left, right = 0, n
 
-        result = []
+        res = []
         while top < bot and left < right:
             for j in range(top, right):
-                result.append(matrix[top][j])
+                res.append(matrix[top][j])
             top += 1
 
             for i in range(top, bot):
-                result.append(matrix[i][right-1])
+                res.append(matrix[i][right-1])
             right -= 1
 
             if top < bot:
                 for j in range(right-1, left-1, -1):
-                    result.append(matrix[bot-1][j])
+                    res.append(matrix[bot-1][j])
                 bot -= 1
 
             if left < right:
                 for i in range(bot-1, top-1, -1):
-                    result.append(matrix[i][left])
+                    res.append(matrix[i][left])
                 left += 1
-            
-        return result
+
+        return res
