ad35183c54dc361a8838037df4ecf7070dda158d
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-08-25 22:39:56 -0500

    Time: 0 ms (100.00%) | Memory: 18.7 MB (9.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,10 +3,12 @@ class Solution:
         l = 0
         r = len(numbers)-1
 
-        while numbers[l] + numbers[r] != target:
-            if numbers[l] + numbers[r] > target:
+        while l < r:
+            value = numbers[l] + numbers[r]
+            if value == target:
+                return [l+1, r+1]
+
+            if value > target:
                 r -= 1
             else:
                 l += 1
-
-        return [l+1, r+1]
