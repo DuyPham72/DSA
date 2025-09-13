65f094cbf9d6f183fcdb8a7646f931c0af61bb13
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-13 10:22:43 -0500

    Time: 7 ms (81.62%) | Memory: 19.7 MB (55.54%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,20 +1,25 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         m, n = len(mat), len(mat[0])
-        ans = []
-
-        for x in range(m + n - 1):
-            # valid rows i for which j = x - i is inside [0..n-1]
-            start_row = max(0, x - (n - 1))
-            end_row   = min(m - 1, x)
-
-            if x % 2 == 0:
-                # even diagonal: traverse upwards (i decreases)
-                for i in range(end_row, start_row - 1, -1):
-                    ans.append(mat[i][x - i])
+        res=[]
+        r=0
+        c=0
+        for _ in range(m*n):
+            res.append(mat[r][c])
+            if (r+c)%2==0:
+                if c==n-1:
+                    r+=1
+                elif r==0:
+                    c+=1
+                else:
+                    r-=1
+                    c+=1
             else:
-                # odd diagonal: traverse downwards (i increases)
-                for i in range(start_row, end_row + 1):
-                    ans.append(mat[i][x - i])
-
-        return ans
+                if r==m-1:
+                    c+=1
+                elif c==0:
+                    r+=1
+                else:
+                    r+=1
+                    c-=1
+        return res
