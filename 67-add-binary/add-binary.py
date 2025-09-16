a85efaa160b4667431110a31c08030fbd87dc65f
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-16 17:41:24 -0500

    Time: 7 ms (5.18%) | Memory: 17.7 MB (73.58%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,36 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        lena = len(a)
+        lenb = len(b)
+        bigger = a if lena > lenb else b
+
+        temp = 0
+        ans = ''
+
+        i = 0
+        while i < min(lena, lenb):
+            value = int(a[-1-i]) +  int(b[-1-i]) + temp
+
+            if value % 2 == 0:
+                ans += '0'
+            else:
+                ans += '1'
+
+            temp = 0 if value < 2 else 1
+            i += 1
+            
+        while i < max(lena, lenb):
+            value = temp + int(bigger[-1-i])
+            
+            if value % 2 == 0:
+                ans += '0'
+            elif value == 1:
+                ans += '1'
+
+            temp = 0 if value < 2 else 1
+            i += 1
+
+        if temp == 1:
+            ans += '1'
+
+        return ans[::-1]
