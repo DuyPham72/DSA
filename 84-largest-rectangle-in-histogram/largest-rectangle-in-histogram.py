41483de36b5fdddc03a31061bd18d62a90b1b18f
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-17 14:52:47 -0500

    Time: 176 ms (33.22%) | Memory: 37.4 MB (6.10%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -9,9 +9,11 @@ class Solution:
                 idx, height = stack.pop()
                 largest = max(largest, height * (i-idx))
                 temp = idx
+
             stack.append([temp, h])
 
+        n = len(heights)
         for i, h in stack:
-            largest = max(largest, h * (len(heights)-i))
+            largest = max(largest, h * (n-i))
 
         return largest
