8a9a60e7db8bcba7ac02158d1926005d0cf140ff
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-17 00:20:11 -0500

    Time: 35 ms (83.38%) | Memory: 18.2 MB (19.46%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -3,8 +3,8 @@ class Solution:
         l, r= 0, len(s)-1
         while l < r:
             if s[l] != s[r]:
-                str1 = s[:l]+s[l+1:]
-                str2 = s[:r]+s[r+1:]
+                str1 = s[:l] + s[l+1:]
+                str2 = s[:r] + s[r+1:]
                 return str1 == str1[::-1] or str2 == str2[::-1]
 
             l += 1
