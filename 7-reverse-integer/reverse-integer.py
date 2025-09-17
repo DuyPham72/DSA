1c3b21a6006b3d2dc7f30d290b50293c0d397198
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-16 20:03:02 -0500

    Time: 31 ms (93.54%) | Memory: 17.9 MB (17.05%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,10 +1,9 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        temp = list(str(x))
-        sign = 0 if temp[0] != '-' else 1
+        ans = 0
+        if x < 0:
+            ans = int(str(x)[1:][::-1]) * -1
+        else:
+            ans = int(str(x)[::-1])
 
-        for i in range((len(temp)-sign)//2):
-            temp[i+sign], temp[-1-i] = temp[-1-i], temp[i+sign]
-
-        ans = int(''.join(temp))
         return 0 if ans > 2**31 - 1 or ans < -2**31 else ans
