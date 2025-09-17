2d6a3fcf1699553f4dbea0a1fcdf053f7aa8170c
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-16 23:56:57 -0500

    Time: 9 ms (45.11%) | Memory: 18.5 MB (38.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alphanumeric = re.findall(f'[a-z0-9]', s.lower())
-        s = ''.join(alphanumeric)
+        s = ''.join(ch for ch in s if ch.isalnum()).lower()
 
         for i in range(len(s)//2):
             if s[i] != s[-i-1]:
