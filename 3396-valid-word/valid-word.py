ea5d0ced4e42f2feac15089f0a56a08fbb864f5a
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-18 20:29:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (35.65%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        vowel = 'aeiouAEIOU'
+        conso = 'qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM'
+        valid = vowel + conso + '0123456789'
+
+        if len(word) < 3:
+            return False
+
+        count1 = 0
+        count2 = 0
+        for c in word:
+            if c in vowel:
+                count1 +=1
+            if c in conso:
+                count2 += 1
+
+            if c not in valid:
+                return False
+
+        print(count1)
+        print(count2)
+        return True if count1 > 0 and count2 > 0 else False
