852a1b0a5757eb18ad7b504e14977941b505900c
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-21 13:45:00 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.86%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -2,12 +2,11 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         temp_pattern = {}
         sett = set()
-        new_s = s.split(' ')
         
-        if len(pattern) != len(new_s):
+        if len(pattern) != len(s.split()):
             return False
-
-        for w, c in zip(pattern, new_s):
+ 
+        for w, c in zip(pattern, s.split()):
             if w not in temp_pattern and c not in sett:
                 sett.add(c)
                 temp_pattern[w] = c
