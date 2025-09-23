f7ae211905778fbadcf680dd73412dbb2a88367d
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-09-23 18:17:31 -0500

    Time: 48 ms (5.72%) | Memory: 17.6 MB (68.31%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,13 +3,14 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l = 1
-        r = n
+        l, r = 1, n
         while l < r:
-            m = l + ((r-l) // 2)
-            if isBadVersion(m):
-                r = m
+            m = (l+r)//2
+            if isBadVersion(m) == False:
+                l = m+1
             else:
-                l = m + 1
+                if isBadVersion(m-1) == False:
+                    return m
+                r = m-1
 
         return l
