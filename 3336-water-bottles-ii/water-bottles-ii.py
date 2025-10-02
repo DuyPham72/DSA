31b4d57880db7357b23008d33b99f1cf49425b64
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-02 02:03:32 -0500

    Time: 43 ms (29.44%) | Memory: 17.9 MB (5.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.py
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -3,10 +3,8 @@ class Solution:
         ans, empty = numBottles, numBottles
         while empty >= numExchange:
             value, left = divmod(empty, numExchange)
-            print(value, left)
             ans += 1
             empty = (value-1)*numExchange + left + 1
             numExchange += 1
-            print(empty)
 
         return ans
