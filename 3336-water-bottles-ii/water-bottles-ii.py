5a0991589f386668d86bd3aea2f442e7ace42965
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-02 02:06:44 -0500

    Time: 35 ms (85.00%) | Memory: 18 MB (5.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.py
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -2,9 +2,9 @@ class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
         ans, empty = numBottles, numBottles
         while empty >= numExchange:
-            value, left = divmod(empty, numExchange)
+            empty -= numExchange
             ans += 1
-            empty = (value-1)*numExchange + left + 1
+            empty += 1
             numExchange += 1
 
         return ans
