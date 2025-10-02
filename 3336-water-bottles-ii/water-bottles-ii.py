2738a41bc1ccd4481125adc9f3dbedc2f155ef63
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-02 02:08:53 -0500

    Time: 42 ms (36.67%) | Memory: 17.5 MB (98.89%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.py
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
-        ans, empty = numBottles, numBottles
-        while empty >= numExchange:
-            empty -= numExchange
+        ans = numBottles
+        while numBottles >= numExchange:
+            numBottles -= numExchange-1
             ans += 1
-            empty += 1
             numExchange += 1
 
         return ans
