a0f7d171e2e78d01809e85a615a5af7ca0578d68
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-07 16:24:14 -0500

    Time: 123 ms (54.32%) | Memory: 18.2 MB (23.73%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,18 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         temp = {}
-        longest = 0
-        curr_max = 0
-        l = 0
+        longest = curr_max = l = 0
 
-        for r in range(len(s)):
-            temp[s[r]] = temp.get(s[r], 0) + 1
-            curr_max = max(curr_max, temp[s[r]])
+        for r, char in enumerate(s):
+            temp[char] = temp.get(char, 0) + 1
+            curr_max = max(curr_max, temp[char])
 
             while (r-l+1) - curr_max > k:
                 temp[s[l]] -= 1
                 l += 1
 
-            longest = max(longest, (r-l+1))
+            longest = max(longest, r-l+1)
 
         return longest
