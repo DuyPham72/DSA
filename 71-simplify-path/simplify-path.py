eebbd2c79bfc6b13494f9b17206fd0fab12ca8f3
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-20 15:02:55 -0500

    Time: 3 ms (36.69%) | Memory: 17.8 MB (55.07%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        result = re.findall(r'/{1}[a-zA-Z._0-9]+', path)
+        print(result)
+
+        ans = []
+        for p in result:
+            if p == '/..':
+                if len(ans) > 0:
+                    ans.pop() 
+                else:
+                    continue
+            elif p == '/.':
+                continue
+            else:
+                ans.append(p)
+
+        return ''.join(ans) if len(ans) > 0 else '/'
