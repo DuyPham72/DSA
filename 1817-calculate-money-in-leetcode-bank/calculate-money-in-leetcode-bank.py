fb61d89e870480e419e9eaafe163e340cf1273e8
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-10-25 21:55:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -14,7 +14,7 @@ class Solution:
             lower += 1
             i += 1
 
-        for j in range(1, day+1):
-            ans += j+i
+        for j in range(day):
+            ans += j+i+1
 
         return ans
