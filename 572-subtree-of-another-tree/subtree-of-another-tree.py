b4d71a7bd871bbc494012dcdb44cdcea9b7e1664
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2025-11-26 20:47:03 -0600

    Time: 47 ms (36.98%) | Memory: 18.3 MB (17.02%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -4,29 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def compare(root1, root2):
-    if not root1 and not root2:
-        return True
-    if not root1 or not root2:
-        return False
-    if root1.val != root2.val:
-        return False
-
-    left = compare(root1.left, root2.left)
-    right = compare(root1.right, root2.right)
-
-    return left and right
-
 class Solution:
+    def compare(self, root1, root2):
+        if not root1 and not root2:
+            return True
+        if not root1 or not root2:
+            return False
+        if root1.val != root2.val:
+            return False
+
+        return self.compare(root1.left, root2.left) and self.compare(root1.right, root2.right)
+
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not root:
             return False
 
         if root.val == subRoot.val:
-            if compare(root, subRoot):
+            if self.compare(root, subRoot):
                 return True
 
         left = self.isSubtree(root.left, subRoot)
         right = self.isSubtree(root.right, subRoot)
-
         return left or right
