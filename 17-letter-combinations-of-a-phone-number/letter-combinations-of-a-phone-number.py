524b9da93a6144e44165194c43f7f1b86eebe2b3
Duy Pham <64452593+DuyPham72@users.noreply.github.com>
2026-01-01 10:49:23 -0600

    Time: 0 ms (100.00%) | Memory: 17.5 MB (81.61%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
+        dicts = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
 
-        dic = { '2': 'abc',
-                '3': 'def',
-                '4': 'ghi',
-                '5': 'jkl',
-                '6': 'mno',
-                '7': 'pqrs',
-                '8': 'tuv',
-                '9': 'wxyz'}
-
+        ans = []
         n = len(digits)
-        result, temp = [], []
-
-        def backtrack(i):
+        def backtrack(temp=[], start=0):
             if len(temp) == n:
-                result.append(''.join(temp))
+                ans.append(''.join(temp))
                 return
 
-            for letter in dic[digits[i]]:
-                temp.append(letter)
-                backtrack(i+1)
-                temp.pop()
-
-        backtrack(0)
-        return result
+            for i in range(start, n):
+                letters = dicts.get(digits[i])
+                for letter in letters:
+                    temp.append(letter)
+                    backtrack(temp, i+1)
+                    temp.pop()
+        
+        backtrack()
+        return ans
